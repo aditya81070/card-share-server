91d214536a03d5416bb60cfc7d2dadd75c4874fc
amritsrivastava <srivastava.amrit1999@gmail.com>
2019-06-29 11:21:56 +0530

    remove card-list and card models and apis

--- src/api/models/index.ts
+++ src/api/models/index.ts
@@ -1,3 +1,1 @@
 export const User = require('./user.model');
-export const Card = require('./card.model');
-export const CardList = require('./card-list.model');
--- src/api/routes/v1/index.ts
+++ src/api/routes/v1/index.ts
@@ -4,10 +4,8 @@ import { apiJson } from 'api/utils/Utils';
 export {};
 
 const userRoutes = require('./user.route');
-const cardRoutes = require('./card.route');
 const authRoutes = require('./auth.route');
 const uploadRoutes = require('./upload.route');
-const cardListRoutes = require('./card-list.route');
 
 const router = express.Router();
 
@@ -25,8 +23,6 @@ router.get('/status', (req, res, next) => {
 router.use('/docs', express.static('docs'));
 
 router.use('/users', userRoutes);
-router.use('/cards', cardRoutes);
-router.use('/cards-list', cardListRoutes);
 router.use('/auth', authRoutes);
 router.use('/upload', uploadRoutes);
 
